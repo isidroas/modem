6796218013a4af9a329c1c2804bbd738f8d7e356
Isidro Arias <isidro.arias@wslw.es>
2022-07-11 14:09:36 +0200

    logging Please enter the commit message for your changes. Lines starting

--- script_pyserial.py
+++ script_pyserial.py
@@ -5,12 +5,14 @@ if __name__=='__main__':
     ser= serial.Serial('COM20', baudrate=19600, timeout=20)
 
     def getc(size, timeout=0):
-        return ser.read(size)
+        read= ser.read(size)
+        print('[main.getc] read=0x'+ read.hex().upper())
 
 
     def putc(data, timeout=0):
         ser.write(data)
         ser.flushOutput()
+        print('[main.putc] write=0x'+ data.hex().upper())
         return len(data)
     
 
